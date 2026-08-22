3a8808f0d06c4c3bb6b0468b6fb80b7036fb1d6f
Saeed Nemati <sn.1361@gmail.com>
2026-08-22 14:56:26 +0330

    committed by script

---
+++ panel/admin/breadcrumb.jsx
@@ -0,0 +1,8 @@
+import PartBreadcrumb from 'partBreadcrumb'
+
+export default props => <PartBreadcrumb
+    icon='eventAvailable'
+    name='appointments'
+    title='appointmentsAppointments'
+    {...props}
+/>
