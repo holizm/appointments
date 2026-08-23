7597687dff40e8527c8ce469bf9cf7b497d2c08f
Saeed Nemati <sn.1361@gmail.com>
2026-08-23 12:39:21 +0330

    committed by script

--- site/parts/appointment.jsx
+++ site/parts/appointment.jsx
@@ -1,6 +1,6 @@
 export default ({ appointment }) => <article class='appointment'>
-    <h2 class='service'>{appointment.service?.title}</h2>
-    <time class='startDate'>{appointment.startDate}</time>
-    <span class='provider'>{appointment.provider?.title}</span>
-    <span class='status'>{appointment.appointmentStatus}</span>
+    <h2 class='service'>{appointment?.service?.title}</h2>
+    <time class='startDate'>{appointment?.startDate}</time>
+    <span class='provider'>{appointment?.provider?.title}</span>
+    <span class='status'>{appointment?.appointmentStatus}</span>
 </article>
